71b742618c9678c299866d42fcad01e8bcbe2bfc
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2026-02-12 15:42:10 +0530

    Haneef Correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Layout from "./pages/Layout";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customer";
-import Vehicle from "./pages/VehicleDetails";
-import Booking from "./pages/Payment";
-import ServiceRepairUpdate from "./pages/ServiceUpdate";
-import Reports from "./pages/Reports";
-import VehicleMaster from "./pages/VehicleMaster";
+import Login from "./pages/common/Login";
+import Layout from "./layout/Layout";
+import Customers from "./pages/admin/Customer";
+import VehicleMaster from "./pages/admin/VehicleMaster";
 
 function App() {
 
@@ -16,13 +11,8 @@ function App() {
 		<Routes>
 			<Route path="/" element={<Login />} />
 			<Route path="/layout" element={<Layout />}>
-				<Route path="dashboard" element={<Dashboard />} />
 				<Route path="customers" element={<Customers />} />
 				<Route path="vechicleMaster" element={<VehicleMaster />} />
-				<Route path="payment" element={<Booking />} />
-				<Route path="service-update" element={<ServiceRepairUpdate />} />
-				<Route path="reports" element={<Reports />} />
-
 			</Route>
 		</Routes>
 	)
